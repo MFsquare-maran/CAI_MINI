5bd726ed22d81d81409b906e5c7d71f2590a787a
Maran Friedli <76904615+MFsquare-maran@users.noreply.github.com>
2026-09-14 11:56:07 +0200

    add credentials.h

---
+++ src/LORA_GATEWAY/credentials.h
@@ -0,0 +1,12 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// ── WLAN ─────────────────────────────────────────────
+#define WLAN_SSID      "MeinWLAN"
+#define WLAN_PW        "MeinPasswort"
+
+// ── ThingsBoard ──────────────────────────────────────
+// Access-Token des Gateway-Devices in iot.mfsquare.ch
+#define GATEWAY_TOKEN  "xxxxxxxxxxxxxxxxxxxx"
+
+#endif // CREDENTIALS_H
